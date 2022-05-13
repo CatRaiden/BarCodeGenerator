b6805ec7ce6738ad66ccd492e7ea5925cb8650a9
KevinSie <kevinsie223@gmail.com>
2022-05-13 18:17:00 +0800

    Initial Commit

---
+++ BarCodeGenerator/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  BarCodeGenerator
+//
+//  Created by 1 on 2022/5/13.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
